24c0e04d376fb9a181d2c69b141eadb8f9a26b7b
Devante W <williams.devante05@gmail.com>
2021-03-17 20:01:35 -0500

    first commit

---
+++ client/build/precache-manifest.c822a699c2e040a6caed22845dea4cf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab65082fab274a884d51823d2e02c86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2175e874f444ea65cf2b",
+    "url": "/static/css/main.997d4878.chunk.css"
+  },
+  {
+    "revision": "dcd954fd859b7c60d919",
+    "url": "/static/js/2.e265699e.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e265699e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2175e874f444ea65cf2b",
+    "url": "/static/js/main.da8670b2.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
